134f9ecdcf6507f4940f954642ef1baf08c689db
itsdabatt <batmannttv@gmail.com>
2026-08-24 14:29:48 -0400

    Bust cache for repeat prayer request fix

--- app-loader.js
+++ app-loader.js
@@ -1,6 +1,6 @@
 /* Loader for the full CMBC interactive app. */
 (function(){
-  const version='20260824memory1';
+  const version='20260824prayer2';
   if(!document.querySelector('script[data-tailwind-cmbc]')){
     const tw=document.createElement('script');tw.src='https://cdn.tailwindcss.com';tw.dataset.tailwindCmbc='1';document.head.appendChild(tw);
   }
